0fbf9bd05420da9409463ac9081971c7dd4746ac
Keiji, Yoshimi <walf443@gmail.com>
2010-04-09 00:14:22 +0900

    added pod.

--- lib/Plack/Middleware/DoCoMoGUID/CheckParam.pm
+++ lib/Plack/Middleware/DoCoMoGUID/CheckParam.pm
@@ -17,3 +17,36 @@ sub call {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::DoCoMoGUID::CheckParam - redirect to param append location if required param is not exist.
+
+=head1 SYNOPSIS
+
+    use Plack::Builder;
+
+    builder {
+        enable_if { $_[0]->{HTTP_USER_AGENT} =~ /DoCoMo/i } 'DoCoMoGUID::CheckParam', params => { guid => 'ON' };
+        # if you are not specify params parameter, params is handled by { guid => 'ON' }.
+    };
+
+=head1 DESCRIPTION
+
+Plack::Middleware::DoCoMoGUID::CheckParam is a Plack::Middleware that redirect to param append location if required param is not exist.
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
++<Plack::Middleware>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
