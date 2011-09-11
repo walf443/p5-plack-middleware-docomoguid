4b5b2ad9d2ac0397c47d1612a17a7df98d7c73ca
Keiji, Yoshimi <walf443@gmail.com>
2011-09-12 08:54:25 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c25eae1..b767db5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,7 @@
     Revision history for Perl extension Plack::Middleware::DoCoMoGUID
    
    +0.06    Mon Sep 12 08:53:57 JST 2011
    +        - fixed packaging (thanks tokuhirom )
    
     0.05    Wed Jan 19 00:21:35 JST 2011
             - recalclate Content-Length header when P::M::DocomoGUID::HTMLStickyQuery change content body. ( thanks takah0918.)

--- lib/Plack/Middleware/DoCoMoGUID.pm
+++ lib/Plack/Middleware/DoCoMoGUID.pm
@@ -2,7 +2,7 @@ package Plack::Middleware::DoCoMoGUID;
 
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use parent 'Plack::Middleware';
 
 use Plack::Middleware::DoCoMoGUID::HTMLStickyQuery;
